b6f15fb17b5321333cfde32dbd7c53a0e96fffa4
jmulq <james.mulqueeny@outlook.com>
2023-08-03 19:34:19 +0100

    feat: add IDKitWidget

--- pages/client.tsx
+++ pages/client.tsx
@@ -1,6 +1,45 @@
-import Layout from "../components/layout"
+import { NextApiResponse } from "next";
+import Layout from "../components/layout";
+import { CredentialType, IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
 
 export default function ClientPage() {
+  const appId = process.env.NEXT_PUBLIC_APP_ID;
+
+  if (!appId) {
+    return <>No app ID. Redeploy with correct env variables</>;
+  }
+
+  const onSuccess = (result: ISuccessResult) => {
+    console.log("onSuccess called with: ", result);
+  };
+
+  const handleVerify = (result: ISuccessResult) => {
+    console.log("handleVerify called with: ", result);
+    validateProof(result);
+  };
+
+  const validateProof = async (res: ISuccessResult) => {
+    const reqBody = {
+      merkle_root: res.merkle_root,
+      nullifier_hash: res.nullifier_hash,
+      proof: res.proof,
+      credential_type: res.credential_type,
+      action: 'election_vote', // or get this from environment variables,
+      signal: 'yes', // if we don't have a signal, use the empty string
+    };
+
+    const response = await fetch('/api/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reqBody),
+    });
+
+    const data = await response.json();
+    console.log('data', data)
+  };
+
   return (
     <Layout>
       <h1>Client Side Rendering</h1>
@@ -22,6 +61,19 @@ export default function ClientPage() {
         The disadvantage of <strong>useSession()</strong> is that it requires
         client side JavaScript.
       </p>
+
+      <IDKitWidget
+        app_id={appId} // obtained from the Developer Portal
+        action="election_vote" // this is your action name from the Developer Portal
+        signal="yes"
+        onSuccess={onSuccess} // callback when the modal is closed
+        handleVerify={handleVerify} // optional callback when the proof is received
+        credential_types={["orb", "phone"] as CredentialType[]} // optional, defaults to ['orb']
+        enableTelemetry // optional, defaults to false
+        // theme="dark"
+      >
+        {({ open }) => <button onClick={open}>Cast a vote</button>}
+      </IDKitWidget>
     </Layout>
-  )
+  );
 }
